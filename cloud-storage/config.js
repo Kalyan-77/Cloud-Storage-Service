6d2eb08569e6bfe91329f40794c9eb03d2cf246c
kalyan <your-email@example.com>
2025-11-29 13:54:28 +0530

    Front - Login Chnage

--- cloud-storage/config.js
+++ cloud-storage/config.js
@@ -1,1 +1,1 @@
-export const BASE_URL = import.meta.env.VITE_API_URL || "https://cloud-storage-app-xzwx.onrender.com";
+export const BASE_URL = import.meta.env.VITE_API_URL || "https://cloud-storage-api-9hjh.onrender.com";
